c56b94e1994e7e2b33a64c96aa241ae6e46d954d
Miki <warlord_bv@yahoo.com>
2014-11-12 22:32:02 +0200

    curs3

---
+++ curs3/js/scripts.js
@@ -0,0 +1,30 @@
+console.log("Hello World!");
+15
+6.3
+934.4, 34, 45.67;
+
+"this is a string";
+"this is a \"formatted\" string";
+
+var oneInteger = 99;
+var oneString = "99";
+console.log(oneInteger == oneString);
+console.log(oneInteger === oneString);
+
+var concatenatedVars = oneInteger + oneString;
+console.log(concatenatedVars);
+
+var hello = "Hello";
+var myName = "Miki";
+console.log(hello + " " + myName);
+
+
+/**
+ * IF
+ */
+if (oneInteger != oneString) {
+    alert("enervant");
+} else {
+    console.log("they are equal");
+}
+
