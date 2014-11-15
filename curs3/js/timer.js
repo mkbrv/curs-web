824b5b4187357cf9e5feab52ca3412a6ec6db495
Miki <warlord_bv@yahoo.com>
2014-11-15 16:17:53 +0200

    added comments to timer.js

--- curs3/js/timer.js
+++ curs3/js/timer.js
@@ -1,56 +1,64 @@
-(function (global) {
+(function (contextGlobal) {
 
     var button = document
-        .getElementById("startTimer");
+        .getElementById("startTimer"); //preluam din DOM elementul cu id= "startTimer"
 
-    var Timer = function (targetDate) {
+    var Timer = function (targetDate) { //constructorul obiectului Timer
         this.targetDate = targetDate;
         this.start = function () {
-            this.log("wasStarted");
-            this.timeOut = setInterval(function () {
-                var currentDate = new Date();
+            this.log("wasStarted");//logam interactiunea
+            this.timeOut = setInterval(function () {//creem o functie anonima care sa se execute every 1000 ms
+                var currentDate = new Date(); // data curenta.
                 var differenceDate = new Date(
-                    targetDate - currentDate
+                    targetDate - currentDate//va crea o noua data din diferenta celorlalte 2. deci probabil vorbim de
+                    //ceva gen ianuarie 1970
                 );
                 var contentDiv =
                     document.getElementById("content");
                 contentDiv.innerHTML =
-                    differenceDate.getDate() + "-" +
+                    differenceDate.getDate() + "-" + //afisam ziua / ora / minute / secunde
                         differenceDate.getHours() + ":" +
                         differenceDate.getMinutes() + ":" +
                         differenceDate.getSeconds();
             }, 1000);
         };
         this.stop = function () {
-            this.log("wasStoped");
-            clearInterval(this.timeOut);
+            this.log("wasStoped");//logam interactiunea
+            clearInterval(this.timeOut);//oprim intervalul
         };
 
         this.log = function (toLog) {
-            if (global.LOGGER !== undefined
-                && global.LOGGER === true) {
+            if (contextGlobal.LOGGER !== undefined//avem o variabila global definita, in functie de care afisam sau nu un mesaj
+                && contextGlobal.LOGGER === true) {//aceasta variabila se poate seta din consola.
+                // exemplu F12 click pe consola, tastam:  LOGGER= true;
+                //dam click pe buton si vom vedea niste mesaje
                 console.log(toLog);
             }
         };
     };
 
-    var timer;
-    button.addEventListener("contextmenu",
+    var timer; //EVENIMENT
+    button.addEventListener("contextmenu",//contextmenu = click dreapta
         function (e) {
             e.preventDefault();
             timer = new Timer(
                 new Date(
                     2014, 12, 1,
-                    20, 0, 0
+                    20, 0, 0//data tinta 1 dec 2014
                 )
             );
-            timer.start();
-        });
-    button.addEventListener("click",
+            timer.start();//pornim timer
+        });//EVENIMENT
+    button.addEventListener("click", // atasam butonului o functie pe evenimentul CLICK
         function (e) {
             if (timer !== undefined) {
-                timer.stop();
+                timer.stop();//oprim timer
             }
         });
 
+    contextGlobal.TIMER = Timer;  //exportam in context global doar 1 singur obiect -- Timer
+
 }(window));
+//window este variabila globala cu care apelam functia anonima precedent declarata
+
+
